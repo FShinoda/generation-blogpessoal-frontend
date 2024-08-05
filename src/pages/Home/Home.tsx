0341ab14a4876e9dca29fa04a58b06ee01a75a36
FShinoda <fernanda.shinoda@hotmail.com>
2024-08-05 15:08:07 -0300

    feat: first component creation

---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,12 @@
+import './Home.css';
+
+function Home() {
+  return (
+    <div>
+      <h1 className="home-title">Home</h1>
+      <img src="https://i.imgur.com/H88yIo2.png" alt="home page image" className="home-img"/>
+    </div>
+  );
+}
+
+export default Home;
