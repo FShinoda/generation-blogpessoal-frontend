d5bd66afce3cfa8c67be1156b2272b6f6ce068d1
FShinoda <fernanda.shinoda@hotmail.com>
2024-08-09 08:18:32 -0300

    feat: register & login page logic done

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,32 @@
-import React from 'react';
 import './App.css';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import NavBar from './components/NavBar/NavBar';
-import Home from './pages/Home/Home';
-import Login from './pages/Login/Login';
+
+import Navbar from './components/NavBar/NavBar';
 import Footer from './components/Footer/Footer';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Login from './pages/Login/Login';
+import Cadastro from './pages/Cadastro/Cadastro';
+import Home from './pages/Home/Home';
+import { AuthProvider } from './contexts/AuthContext';
+
 
 function App() {
   return (
     <>
-    <BrowserRouter>
-        <NavBar />
+    <AuthProvider>
+        <BrowserRouter>
+          <Navbar />
           <div className='min-h-[80vh]'>
             <Routes>
-              <Route path="/" element={<Home />} />
+              <Route path="/" element={<Login />} />
               <Route path="/login" element={<Login />} />
+              <Route path="/cadastro" element={<Cadastro />} />
               <Route path="/home" element={<Home />} />
             </Routes>
           </div>
           <Footer />
         </BrowserRouter>
+    </AuthProvider>
     </>
-);
+  );
 }
 export default App;
