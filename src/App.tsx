de6e1dab58811bb22ebc9d5c1df4fc908adedd76
FShinoda <fernanda.shinoda@hotmail.com>
2024-08-12 15:32:32 -0300

    Tema CRUD done

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,9 @@ import Login from './pages/Login/Login';
 import Cadastro from './pages/Cadastro/Cadastro';
 import Home from './pages/Home/Home';
 import { AuthProvider } from './contexts/AuthContext';
+import ListaTemas from './components/Temas/ListaTemas/ListaTemas';
+import FormularioTema from './components/Temas/FormularioTema/FormularioTema';
+import DeletarTema from './components/Temas/DeletarTema/DeletarTema';
 
 
 function App() {
@@ -21,11 +24,15 @@ function App() {
               <Route path="/login" element={<Login />} />
               <Route path="/cadastro" element={<Cadastro />} />
               <Route path="/home" element={<Home />} />
+              <Route path="/temas" element={<ListaTemas />} />
+              <Route path="/cadastroTema" element={<FormularioTema />} />
+              <Route path="/editarTema/:id" element={<FormularioTema />} />
+              <Route path="/deletarTema/:id" element={<DeletarTema />} />
             </Routes>
           </div>
           <Footer />
         </BrowserRouter>
-    </AuthProvider>
+        </AuthProvider>
     </>
   );
 }
